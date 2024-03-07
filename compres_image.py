85f27aa44aed8daf1c25ea0639d3ceafb6e7bea3
Anton <antonjan@users.noreply.github.com>
2024-03-07 18:52:26 +0200

    Create compres_image.py

---
+++ compres_image.py
@@ -0,0 +1,7 @@
+
+from PIL import Image
+file_path =  "image_uncompressed.jpg"
+img = Image.open(file_path)
+height, width = img.size
+compressed = img.resize((height, width), Image.ANTIALIAS)
+compressed.save("image_compressed.jpg", optimize=True,quality=9)
