c1d6eb00e42285946f63f74b2b1acb2d65234d75
Anton <antonjan@users.noreply.github.com>
2024-03-13 16:33:45 +0200

    adding barcode images for testing barcode reader

--- barcode_reader.py
+++ barcode_reader.py
@@ -1,11 +1,6 @@
-
-"""
-Created on Thu Dec 28 12:55:48 2023
-@author: mh.olyaei@yahoo.com
-"""
 from pyzbar.pyzbar import *
 import cv2
-img = cv2.imread("barcode(3).png")
+img = cv2.imread("barcode4.png")
 Barcode = decode(img)
 print("Decode:", Barcode[0].data)
 cv2.imshow("img", img)
