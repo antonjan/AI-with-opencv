dcacf5e04db3df61dbb2425eba3f4128e9ad1a9f
Anton <antonjan@users.noreply.github.com>
2024-03-13 15:37:33 +0200

    Adding video file for testing

--- car_detector.py
+++ car_detector.py
@@ -1,13 +1,13 @@
 import cv2
-cap = cv2.VideoCapture('video.avi')
+cap = cv2.VideoCapture('traffic_-_27260 (720p).mp4.avi')
 car_cascade = cv2.CascadeClassifier('car.xml')
 while True:
     ret, frames = cap.read()
     gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
     cars = car_cascade.detectMultiScale(gray, 1.1, 1) 
     for (x,y,w,h) in cars:
-        cv2.rectangle(frames,(x,y),(x+w,y+h),(255,0,0),2)
+       cv2.rectangle(frames,(x,y),(x+w,y+h),(255,0,0),2)
     cv2.imshow('video', frames)
     if cv2.waitKey(30) == 27:
-        break
+       break
 cv2.destroyAllWindows()
