f465aa5747bc59b03a67f30e8e2381b5ac890783
Anton <antonjan@users.noreply.github.com>
2024-03-13 16:14:28 +0200

    adding audio file and fixed audio playing command

--- Text-to-Speech.py
+++ Text-to-Speech.py
@@ -1,6 +1,12 @@
 from gtts import gTTS
-import os
-tts = gTTS(text="Hello. Join telegram channel @OpenCV_olc", lang='en')
+#import os
+#import vlc
+import subprocess
+result = subprocess.run(['ls', '-l'])
+tts = gTTS(text="Hello. This is anton speeking what do you want to do today", lang='en')
 tts.save("out.mp3")
-  
-os.system("out.mp3")
+result = subprocess.run(['ffplay','out.mp3'])
+
+#p = vlc.MediaPlayer("out.mp3")
+#p.play()
+#os.system(ffplay "out.mp3")
