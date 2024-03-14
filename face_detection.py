411ee6b8c696e8883dafa5eda2ece86bcc29cc51
Anton <antonjan@users.noreply.github.com>
2024-03-14 09:32:20 +0200

    fix space delimiter and uploading images to be used with face detection

--- face_detection.py
+++ face_detection.py
@@ -1,8 +1,8 @@
 import cv2
-model = cv2.CascadeClassifier('face_detector.xml')
-img = cv2.imread('image.jpg')
+model = cv2.CascadeClassifier('face_default.xml')
+img = cv2.imread('face1.png')
 faces = model.detectMultiScale(img, 1.1, 4)
 for (x, y, w, h) in faces: 
-  cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+  cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 cv2.imwrite("face_detected.png", img) 
 print('output saved')
