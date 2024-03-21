60cd3c8025aced2a3edd1a9e245dc64e9f762d3f
Anton <antonjan@users.noreply.github.com>
2024-03-21 08:32:20 +0200

    Add files via upload
    Adding file name as input parameters

--- pdf_to_img_ocr_text/img_pdf_langchange_to_text.py
+++ pdf_to_img_ocr_text/img_pdf_langchange_to_text.py
@@ -2,6 +2,7 @@
 #import UnstructuredFileLoader
 from langchain_community.document_loaders import UnstructuredFileLoader
 from langchain.document_loaders.image import UnstructuredImageLoader
+import sys
 
 def extract_text_with_langchain_image(list_dict_final_images):
     
@@ -32,7 +33,19 @@ def extract_text_with_langchain_pdf(pdf_file):
     
     return pdf_pages_content
 
-text_with_langchain_files = extract_text_with_langchain_pdf("img_pdf.pdf")
+
+args = len(sys.argv) - 1
+pos = 1
+while (args >= pos):
+    print ("Parameter at position %i is %s" % (pos, sys.argv[pos]))
+    pos = pos + 1
+    #text_with_langchain_files = extract_text_with_langchain_pdf("img_pdf.pdf")
+    text_with_langchain_files = extract_text_with_langchain_pdf(sys.argv[1])
+    f= open(sys.argv[2],"a+")
+    f.write(text_with_langchain_files)
+f.close()    
+print ("The script was called with %i arguments" % (args))
+
 print(text_with_langchain_files)
 
 
